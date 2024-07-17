d28055c62498009a8f89c1cb9531c8d51e0ce5ee
Marshal10 <marshalmaleane123@gmail.com>
2024-07-17 14:00:28 +0530

    Finish screen ,progress bar and highscore state

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,4 +1,4 @@
-function FinishScreen({ points, maxPossiblePoints }) {
+function FinishScreen({ points, maxPossiblePoints, highscore }) {
   const percentage = (points / maxPossiblePoints) * 100;
   let emoji;
   if (percentage === 100) emoji = "🥇";
@@ -12,6 +12,7 @@ function FinishScreen({ points, maxPossiblePoints }) {
         <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
         {maxPossiblePoints} ({Math.ceil(percentage)}%)
       </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
       <button className="btn btn-ui">Restart Quiz</button>
     </>
   );
---
+++ src/components/Progress.js
@@ -0,0 +1,18 @@
+function Progress({ index, numQuestions, points, maxPossiblePoints, answer }) {
+  return (
+    <header className="progress">
+      <progress
+        max={numQuestions}
+        value={index + Number(answer !== null)}
+      ></progress>
+      <p>
+        Question <strrong>{index + 1}</strrong>/{numQuestions}
+      </p>
+      <p>
+        <strong>{points}</strong>/{maxPossiblePoints}
+      </p>
+    </header>
+  );
+}
+
+export default Progress;
